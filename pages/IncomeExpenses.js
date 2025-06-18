254be445413e40f85b0dfdc64ae2b15884bb6133
Danieltash0 <danieltasho11@gmail.com>
2025-06-18 05:51:09 +0300

    check

--- pages/IncomeExpenses.js
+++ pages/IncomeExpenses.js
@@ -5,7 +5,7 @@ export default function IncomeExpenses() {
   return (
     <div className="income-expenses">
       <h2>Income & Expenses</h2>
-      <p>Track income and expense records of the farm.</p>
+      <p>Track income and expense records of the cattle farm.</p>
     </div>
   );
 }
